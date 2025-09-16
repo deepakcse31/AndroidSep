3e73ba0b77b4c01725af95f2f76cb018b8a19542
Deepak <deepakcse31@gmail.com>
2025-09-16 20:42:25 +0530

    app icon/Login Validation

--- app/src/main/java/com/example/androidsep/LoginActivity.java
+++ app/src/main/java/com/example/androidsep/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.androidsep;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +15,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edtNo,edtpass;
+    Button btnlogin;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        edtNo=findViewById(R.id.edtphone);
+        edtpass=findViewById(R.id.edtpassword);
+        btnlogin=findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String no=edtNo.getText().toString();
+                String pass=edtpass.getText().toString();
+                if (no.equals("9876543210") && pass.equals("12345"))
+                {
+                    Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
